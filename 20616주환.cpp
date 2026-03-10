dd60886c497140596c94013b477c3a585c4ca593
jhjw0903 <dj20250618@dge.go.kr>
2026-03-10 15:08:19 +0900

    Add files via upload

---
+++ 20616주환.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	printf("정수를 입력하세요 : ");
+	scanf("%d", &a);
+	
+	if(a>0){
+		printf("양수입니다.");
+    } else if(a<0){
+    	printf("음수입니다.");
+}else {
+	printf("0입니다.");
+} 
+	} 
+
+
